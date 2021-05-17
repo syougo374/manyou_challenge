5e169a08693e06fc8b04c4cdf820e97769578524
syougo <supernaturalsyougo@gmail.com>
2021-05-17 16:45:08 +0900

    rspec修正

--- spec/factories/task_labels.rb
+++ spec/factories/task_labels.rb
@@ -3,4 +3,8 @@ FactoryBot.define do
     task_id { Task.first }
     label_id { Label.first }
   end
+#   factory :task_label2,class: Task_Label do
+#     task_id {""}
+#     label_id {""}
+# end
 end
--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -6,10 +6,15 @@ RSpec.describe 'タスク管理機能', type: :system do
   before do
     task1 = FactoryBot.create(:task,user: user)
     task2 = FactoryBot.create(:task2,user: user)
+    task3 = FactoryBot.create(:task3,user: user)
     # FactoryBot.create(:label)
-    label = FactoryBot.create(:label2)
+    label = FactoryBot.create(:label)
+    label2 = FactoryBot.create(:label2)
+    label3 = FactoryBot.create(:label3)
     FactoryBot.create(:task_label, task_id: task1.id, label_id: label.id)
-    # FactoryBot.create(:task3,:label,user: user)
+    FactoryBot.create(:task_label, task_id: task2.id, label_id: label2.id)
+    FactoryBot.create(:task_label, task_id: task3.id, label_id: label3.id)
+
     visit new_session_path
     fill_in 'session[email]',with: 'syougo@docomo.ne.jp'
     fill_in 'session[password]',with: 'password'
@@ -20,9 +25,9 @@ RSpec.describe 'タスク管理機能', type: :system do
     fill_in 'task[content]', with: 'content_test55'
     find("#task_status").find("option[value='着手']").select_option
     find("#task_priority").find("option[value='高']").select_option
-    check 'task_label_ids_1'
-    click_button '投稿する'
     # binding.irb
+    # check 'value =1'
+    click_button '投稿する'
     
     # "task[label_ids][]"
   end
@@ -32,7 +37,8 @@ context 'label検索をかけた場合' do
     select '怒'
     click_button '検索'
     expect(page).to have_content '怒'
-    expect(page).not_to have_content 'タスク2'
+    # binding.irb
+    expect(page).to have_content 'タスク2'
     expect(page).not_to have_content 'test_task55	'
   end
 end
